6e29ae1572ef773de687c69e5a293704757ea740
omar Hamdan <hamdano@mcmaster.ca>
2022-09-04 16:41:05 -0400

    testing build with modifications

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,7 @@
 <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
   <div class="mx-auto max-w-screen-xl text-center">
       <a href="/" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-          LWView test
+          LWView Rest
       </a>
       <p class="my-6 text-gray-500 dark:text-gray-400">Build your web presence here for the long run.</p>
       <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
